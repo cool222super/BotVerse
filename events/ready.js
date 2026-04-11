2797ac7ac497719fdd03b94a5c76989a95731fb7
Super <168615716+cool222super@users.noreply.github.com>
2026-04-11 00:24:15 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,7 @@
-
-
-
-// Before you use this code please know that this code is old and might have errors in it and I do not expect people saying that this code is great and all. But I understand!
+// Before you use this code please know that this code is old and might have errors in it.
+// I don’t guarantee it’s perfect, but it should work fine.
 
 // Made by Supercoolsbro :D
-
-
 
 const { ActivityType } = require('discord.js');
 const mongoose = require('mongoose');
